001e40a30a79aad3e76f0f709c52d7bc01a4eff1
AMaaryam <a.maaryam@gmail.com>
2024-01-04 22:46:12 +0100

    request

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,4 +9,4 @@ echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
+sudo service nginx start.
